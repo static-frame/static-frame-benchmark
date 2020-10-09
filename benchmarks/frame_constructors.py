01b123a28c6c9a4f368a49053cfdd59eaa9f3487
Christopher Ariza <ariza@flexatone.com>
2020-10-09 11:43:26 -0700

    added basic config

---
+++ benchmarks/frame_constructors.py
@@ -0,0 +1,23 @@
+
+
+import static_frame as sf
+
+class FromRecords:
+
+    # params = [None, 1000]
+    # param_names = ["nrows"]
+
+    # # Generators get exhausted on use, so run setup before every call
+    # number = 1
+    # repeat = (3, 250, 10)
+
+    def setup(self, nrows):
+        N = 100000
+        self.gen = ((x, x * 20, x * 100) for x in range(N))
+
+    def time_frame_from_records(self, nrows):
+        f = sf.Frame.from_records(self.gen)
+
+
+
+if __name__ ==
