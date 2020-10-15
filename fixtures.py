fce9a5958a8726363dec7ac54c4a19e7aeb54d61
Christopher Ariza <ariza@flexatone.com>
2020-10-15 09:02:35 -0700

    preliminary implementation of pnf fixtures

--- fixtures.py
+++ fixtures.py
@@ -2,13 +2,17 @@ import typing as tp
 import itertools as it
 import numpy as np
 from static_frame.core.util import DtypeSpecifier
+from static_frame.core.container import ContainerOperand
+
 import static_frame as sf
 import function_pipe as fpn
 # from function_pipe import pipe_node
 # from function_pipe import pipe_node_factory
 
+DtypeSpecOrSpecs = tp.Union[DtypeSpecifier, tp.Tuple[DtypeSpecifier, ...]]
+DTYPE_OBJECT = np.dtype(object)
 
-def dtype_to_element(dtype: np.dtype) -> tp.Iterator[tp.Any]:
+def dtype_to_element_iter(dtype: np.dtype) -> tp.Iterator[tp.Any]:
 
     if dtype.kind == 'i': # int
         return it.cycle((0, -100, 25, -85555555, 23485))
@@ -32,22 +36,39 @@ def dtype_to_element(dtype: np.dtype) -> tp.Iterator[tp.Any]:
     raise NotImplementedError(f'no handling for {dtype}')
 
 
-def dtype_to_array(dtype: np.dtype, count: int) -> np.ndarray:
-    gen = dtype_to_element(dtype)
+def dtype_to_array(
+        dtype: np.dtype,
+        count: int,
+        gen: tp.Optional[tp.Iterator[tp.Any]] = None,
+        ) -> np.ndarray:
+    '''
+    Args:
+        gen: optionally supply a generator of values, to e used instead
+    '''
+    if not gen:
+        gen = dtype_to_element_iter(dtype)
+
     if dtype.kind != 'O':
-        return np.fromiter(gen, count=count, dtype=dtype)
+        array = np.fromiter(gen, count=count, dtype=dtype)
+    else:
+        array = np.empty(shape=count, dtype=dtype) # object
+        for i, v in zip(range(len(array)), gen):
+            array[i] = v
 
-    array = np.empty(shape=count, dtype=dtype) # object
-    for i, v in zip(range(len(array)), gen):
-        array[i] = v
-
+    array.flags.writeable = False
     return array
 
 
 def dtype_spec_to_array(
-        dtype_spec: DtypeSpecifier,
+        dtype_spec: DtypeSpecOrSpecs,
         count: int,
         ) -> np.ndarray:
+
+    if isinstance(dtype_spec, tuple):
+        # an object type of tuples
+        gen = zip(*(dtype_to_element_iter(np.dtype(dts)) for dts in dtype_spec))
+        return dtype_to_array(DTYPE_OBJECT, count=count, gen=gen)
+
     dtype = np.dtype(dtype_spec)
     return dtype_to_array(dtype, count)
 
@@ -64,25 +85,71 @@ IN = sf.IndexNanosecond
 ING = sf.IndexNanosecondGO
 
 
-@fpn.pipe_node_factory
-def f(type_symbol, **kwargs):
-    pni = kwargs[fpn.PN_INPUT]
-    pni['f'] = dict(type_symbol=type_symbol)
+class Builder:
+
+    @staticmethod
+    def build_index(
+            count: int,
+            constructor: tp.Type[ContainerOperand],
+            dtype_spec: DtypeSpecOrSpecs,
+            index_constructors=None,
+            ) -> tp.Union[sf.Index, sf.IndexHierarchy]:
+
+        if issubclass(constructor, IH):
+            # dtype_spec must be a tuple
+            if not isinstance(dtype_spec, tuple) or len(dtype_spec) < 2:
+                raise RuntimeError(f'for building IH dtype_spec must be a tuple')
+            if index_constructors and len(index_constructors) != len(dtype_spec):
+                raise RuntimeError(f'length of index_constructors must be the same as dtype_spec')
+            tb = sf.TypeBlocks.from_blocks(dtype_spec_to_array(dts, count=count)
+                    for dts in dtype_spec)
+            return constructor._from_type_blocks(tb,
+                    index_constructors=index_constructors,
+                    own_blocks=True,
+                    )
+
+        if index_constructors:
+            raise RuntimeError('cannot provide index_constructors if not building an IH index')
+
+        array = dtype_spec_to_array(dtype_spec, count=count)
+        return constructor.from_labels(array)
+
+
+
+
+
 
 @fpn.pipe_node_factory
-def i(type_symbol, values_dtype_spec, **kwargs):
+def f(constructor, **kwargs):
     pni = kwargs[fpn.PN_INPUT]
-    pni['i'] = dict(type_symbol=type_symbol, values_dtype_spec=values_dtype_spec)
+    pni['f'] = dict(constructor=constructor)
 
 @fpn.pipe_node_factory
-def c(type_symbol, values_dtype_spec, **kwargs):
+def i(constructor,
+        dtype_spec: DtypeSpecOrSpecs,
+        index_constructors=None,
+        **kwargs):
     pni = kwargs[fpn.PN_INPUT]
-    pni['c'] = dict(type_symbol=type_symbol, values_dtype_spec=values_dtype_spec)
+    pni['i'] = dict(constructor=constructor,
+            dtype_spec=dtype_spec,
+            index_constructors=index_constructors,
+            )
 
 @fpn.pipe_node_factory
-def v(*values_dtype_spec, **kwargs):
+def c(constructor,
+            dtype_spec: DtypeSpecOrSpecs,
+            index_constructors=None,
+            **kwargs):
     pni = kwargs[fpn.PN_INPUT]
-    pni['v'] = dict(values_dtype_spec=values_dtype_spec)
+    pni['c'] = dict(constructor=constructor,
+            dtype_spec=dtype_spec,
+            index_constructors=index_constructors,
+            )
+
+@fpn.pipe_node_factory
+def v(*dtype_spec, **kwargs):
+    pni = kwargs[fpn.PN_INPUT]
+    pni['v'] = dict(dtype_spec=dtype_spec)
 
 
 class Shape(fpn.PipeNodeInput):
--- test_fixtures.py
+++ test_fixtures.py
@@ -1,19 +1,23 @@
 import numpy as np
 
-from fixtures import dtype_to_element
-from fixtures import dtype_to_array
+import static_frame as sf
 
+from fixtures import dtype_to_element_iter
+from fixtures import dtype_to_array
+from fixtures import dtype_spec_to_array
+
+from fixtures import Builder
 
 
 # pytest -s --color no --disable-pytest-warnings --tb=native
 
-def test_dtype_to_element_a():
-    gen = dtype_to_element(np.dtype(int))
+def test_dtype_to_element_iter_a() -> None:
+    gen = dtype_to_element_iter(np.dtype(int))
     post = [v for _, v in zip(range(8), gen)]
     assert post == [0, -100, 25, -85555555, 23485, 0, -100, 25]
 
 
-def test_dtype_to_array_a():
+def test_dtype_to_array_a() -> None:
     a1 = dtype_to_array(np.dtype(int), 4)
     assert a1.tolist() == [0, -100, 25, -85555555]
 
@@ -21,10 +25,27 @@ def test_dtype_to_array_a():
     assert a2.tolist() == [None, 'foo', 2.5, False]
 
 
+def test_spec_to_array_a() -> None:
+
+    a1 = dtype_spec_to_array((int, bool, str), 4)
+    assert a1.shape == (4,)
+    assert a1[0] == (0, False, 'foo')
+
     # import ipdb; ipdb.set_trace()
 
 
-def test_node_a():
+def test_build_index_a() -> None:
+
+    ix1 = Builder.build_index(4, sf.IndexHierarchy, (int, bool))
+    assert ix1.shape  == (4, 2)
+
+def test_build_index_b() -> None:
+
+    ix1 = Builder.build_index(4, sf.IndexDate, int)
+    assert ix1.__class__ == sf.IndexDate
+
+
+def test_node_a() -> None:
 
     from fixtures import f, i, c, v
     from fixtures import F, I, Shape
@@ -36,3 +57,5 @@ def test_node_a():
     print(shape)
 
     # import ipdb; ipdb.set_trace()
+
+
