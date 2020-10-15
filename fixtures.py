bb02df6b11c57b86bc2681e3f097dd537f43c329
Christopher Ariza <ariza@flexatone.com>
2020-10-14 21:14:32 -0700

    added fixtures util, test

--- fixtures.py
+++ fixtures.py
@@ -4,25 +4,30 @@ import numpy as np
 from static_frame.core.util import DtypeSpecifier
 
 
-def dtype_spec_to_element(dtype: np.dtype) -> tp.Iterator[tp.Any]:
+def dtype_to_element(dtype: np.dtype) -> tp.Iterator[tp.Any]:
 
     if dtype.kind == 'i': # int
         return it.cycle((0, -100, 25, -85555555, 23485))
     elif dtype.kind == 'u': # int unsigned
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle((0, 100, 25, 512, 234))
     elif dtype.kind == 'f': # float
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle((0.0, 3.2, 3.2e-8, np.nan))
     elif dtype.kind == 'c': # complex
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle((3+5j, np.nan+3j, 100+.0003j))
     elif dtype.kind == 'b': # boolean
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle((False, True))
     elif dtype.kind in ('U', 'S'): # str
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle(('foo', 'foo', 'bar', 'baz'))
     elif dtype.kind == 'O': # object
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle((None, 'foo', 2.5, False))
     elif dtype.kind == 'M': # datetime64
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle((np.datetime64('2012-01-05'), np.datetime64('2015-02-20'), ))
     elif dtype.kind == 'm': # timedelta64
-        return it.cycle((0, -100, 25, -85555555, 23485))
+        return it.cycle((np.timedelta64(3), np.timedelta64(20),))
 
-    raise NotImplementedError()
+    raise NotImplementedError(f'no handling for {dtype}')
+
+
+def dtype_to_array(dtype: np.dtype, size: int) -> np.ndarray:
+    if dtype.kind != 'O':
+        return np.fromiter(dtype_to_element(dtype), size=size, dtype=dtype)
--- test_fixtures.py
+++ test_fixtures.py
@@ -1,5 +1,6 @@
 import numpy as np
 from fixtures import dtype_spec_to_element
+from fixtures import dtype_to_array
 
 # pytest -s --color no --disable-pytest-warnings --tb=native
 
@@ -7,3 +8,8 @@ def test_dtype_to_element_a():
     gen = dtype_spec_to_element(np.dtype(int))
     post = [v for _, v in zip(range(8), gen)]
     assert post == [0, -100, 25, -85555555, 23485, 0, -100, 25]
+
+
+def test_dtype_to_array_a():
+    a1 = dtype_to_array(np.dytpe(str), 4)
+    import ipdb; ipdb.set_trace()
