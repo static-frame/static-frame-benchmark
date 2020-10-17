5073a05ba695bf61a41dd2f8fa7524fbe313ba97
Christopher Ariza <ariza@flexatone.com>
2020-10-17 14:58:25 -0700

    refactored with benchmark reuse through prototype class

--- benchmarks/frame.py
+++ benchmarks/frame.py
@@ -3,110 +3,74 @@ import numpy
 import static_frame as sf
 from hashlib import blake2b
 import pickle
+import inspect
 import os
 from types import SimpleNamespace
 from .fixtures import FixtureFactory
+from .fixtures import ShapeType
 
 
-def set_methods_name(cls):
-
-    for name in dir(cls):
-        if name.startswith('time_'):
-            func = getattr(cls, name)
-            name = f"{name.replace('time_', '')}-{cls.FACTORY_INPUT}"
-            print(cls, name)
-            func.pretty_name = name
-    return cls
-
-# NOTE: could not get setup_cache to work unless implemented explicitly on the derived class; not sure how to get reuse from method; tried assigning in decorator
-
-@set_methods_name
-class FrameA:
-
-    FACTORY_INPUT = 'f(F)|i(I,str)|c(I,str)|v(float)'
-    SHAPE = (1000, 10)
-
-    @classmethod
-    def get_cache_name(cls) -> str:
-        h = blake2b(digest_size=6)
-        h.update(str.encode(cls.FACTORY_INPUT + str(cls.SHAPE)))
-        return h.hexdigest()
-
-    @classmethod
-    def create_fixtures(cls):
-        frame = FixtureFactory.from_str(cls.FACTORY_INPUT)(cls.SHAPE)
-
-        records = [t for t in frame.iter_tuple(axis=1)]
-        records_dict = [dict(t) for t in frame.iter_series(axis=1)]
-        items = [(c, frame[c].values) for c in frame.columns]
-
-        return SimpleNamespace(records=records,
-                records_dict=records_dict,
-                items=items,
-                frame=frame)
-
-    def setup_cache(self) -> SimpleNamespace:
-        return self.create_fixtures()
+class Prototype:
 
     #---------------------------------------------------------------------------
-    def time_from_concat_0(self, ns: SimpleNamespace):
+    def asv_time_from_concat_0(self, ns: SimpleNamespace):
         sf.Frame.from_concat((ns.frame, ns.frame, ns.frame),
                 axis=0,
                 index=sf.IndexAutoFactory)
 
-    def time_from_concat_1(self, ns: SimpleNamespace):
+    def asv_time_from_concat_1(self, ns: SimpleNamespace):
         sf.Frame.from_concat((ns.frame, ns.frame, ns.frame),
                 axis=1,
                 columns=sf.IndexAutoFactory)
 
-    def time_from_concat_items_0(self, ns: SimpleNamespace):
+    def asv_time_from_concat_items_0(self, ns: SimpleNamespace):
         sf.Frame.from_concat_items(enumerate((ns.frame, ns.frame, ns.frame)),
                 axis=0)
 
-    def time_from_concat_items_1(self, ns: SimpleNamespace):
+    def asv_time_from_concat_items_1(self, ns: SimpleNamespace):
         sf.Frame.from_concat_items(enumerate((ns.frame, ns.frame, ns.frame)),
                 axis=1)
 
     # from_overlay: need multiple frames
 
-    def time_from_records(self, ns: SimpleNamespace):
+    def asv_time_from_records(self, ns: SimpleNamespace):
         f = sf.Frame.from_records(ns.records)
 
-    def time_from_dict_records(self, ns: SimpleNamespace):
+    def asv_time_from_dict_records(self, ns: SimpleNamespace):
         f = sf.Frame.from_dict_records(ns.records_dict)
 
     # from_records_items
     # from_dict_records_items
 
-    def time_from_items(self, ns: SimpleNamespace):
+    def asv_time_from_items(self, ns: SimpleNamespace):
         f = sf.Frame.from_items(ns.items)
 
     # from_dict: same as from_items
 
     #---------------------------------------------------------------------------
 
-    def time_iter_array_0(self, ns: SimpleNamespace):
+    def asv_time_iter_array_0(self, ns: SimpleNamespace):
         for part in ns.frame.iter_array(0):
             pass
 
-    def time_iter_array_1(self, ns: SimpleNamespace):
+    def asv_time_iter_array_1(self, ns: SimpleNamespace):
         for part in ns.frame.iter_array(1):
             pass
 
 
-    def time_iter_tuple_0(self, ns: SimpleNamespace):
+    def asv_time_iter_tuple_0(self, ns: SimpleNamespace):
         for part in ns.frame.iter_tuple(0):
             pass
 
-    def time_iter_tuple_1(self, ns: SimpleNamespace):
+    def asv_time_iter_tuple_1(self, ns: SimpleNamespace):
         for part in ns.frame.iter_tuple(1):
             pass
 
-    def time_iter_series_0(self, ns: SimpleNamespace):
+    def asv_time_iter_series_0(self, ns: SimpleNamespace):
         for part in ns.frame.iter_series(0):
             pass
 
-    def time_iter_series_1(self, ns: SimpleNamespace):
+    def asv_time_iter_series_1(self, ns: SimpleNamespace):
         for part in ns.frame.iter_series(1):
             pass
 
@@ -114,31 +78,79 @@ class FrameA:
     # iter_group
     # iter_group_labels
 
-    def time_iter_window_0(self, ns: SimpleNamespace):
+    def asv_time_iter_window_0(self, ns: SimpleNamespace):
         for part in ns.frame.iter_window(size=10, axis=0):
             pass
 
-    def time_iter_window_1(self, ns: SimpleNamespace):
+    def asv_time_iter_window_1(self, ns: SimpleNamespace):
         for part in ns.frame.iter_window(size=10, axis=1):
             pass
 
 
-    def time_iter_window_array_0(self, ns: SimpleNamespace):
+    def asv_time_iter_window_array_0(self, ns: SimpleNamespace):
         for part in ns.frame.iter_window_array(size=10, axis=0):
             pass
 
-    def time_iter_window_array_1(self, ns: SimpleNamespace):
+    def asv_time_iter_window_array_1(self, ns: SimpleNamespace):
         for part in ns.frame.iter_window_array(size=10, axis=1):
             pass
 
-    def time_iter_element(self, ns: SimpleNamespace):
+    def asv_time_iter_element(self, ns: SimpleNamespace):
         for part in ns.frame.iter_element():
             pass
 
-@set_methods_name
-class FrameB(FrameA):
+def apply_prototype(cls_prototype):
+    def decorator(cls):
 
-    FACTORY_INPUT = 'f(F)|i(I,str)|c(I,str)|v(str,float,int,bool)'
+        for name in dir(cls_prototype):
+            if name.startswith('asv_time_'):
+                name_new = name.replace('asv_time_', 'time_')
+                name_pretty = f"{name.replace('asv_time_', '')}-{cls.FIXTURE}"
+
+                # NOTE: must bind Prototype func name at func def time
+                def func_new(self, ns: SimpleNamespace, name_prototype=name):
+                    return getattr(cls_prototype, name_prototype)(self, ns)
+
+                func_new.pretty_name = name_pretty
+                func_new.pretty_source = inspect.getsource(getattr(cls_prototype, name))
+
+                setattr(cls, name_new, func_new)
+        return cls
+    return decorator
+
+
+
+# NOTE: could not get setup_cache to work unless implemented explicitly on the derived class
+# see https://github.com/airspeed-velocity/asv/issues/880
+
+
+def create_fixtures(fixture: str, shape: ShapeType):
+    frame = FixtureFactory.from_str(fixture)(shape)
+
+    records = [t for t in frame.iter_tuple(axis=1)]
+    records_dict = [dict(t) for t in frame.iter_series(axis=1)]
+    items = [(c, frame[c].values) for c in frame.columns]
+
+    return SimpleNamespace(records=records,
+            records_dict=records_dict,
+            items=items,
+            frame=frame)
+
+@apply_prototype(Prototype)
+class FrameA:
+
+    FIXTURE = 'f(F)|i(I,str)|c(I,str)|v(float)'
+    SHAPE = (1000, 10)
 
     def setup_cache(self) -> SimpleNamespace:
-        return self.create_fixtures()
+        return create_fixtures(self.FIXTURE, self.SHAPE)
+
+
+@apply_prototype(Prototype)
+class FrameB:
+
+    FIXTURE = 'f(F)|i(I,str)|c(I,str)|v(str,float,int,bool)'
+    SHAPE = (1000, 10)
+
+    def setup_cache(self) -> SimpleNamespace:
+        return create_fixtures(self.FIXTURE, self.SHAPE)
