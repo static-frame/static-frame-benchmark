3416253fc1339a562b0aa8dbbc5ac568a20efc75
Christopher Ariza <ariza@flexatone.com>
2020-10-18 06:53:12 -0700

    extended benchmarks

--- benchmarks/frame.py
+++ benchmarks/frame.py
@@ -8,6 +8,8 @@ from .prototype import apply_prototype
 class Prototype:
 
     #---------------------------------------------------------------------------
+    # constructors
+
     def asv_time_from_concat_0(self, ns: SimpleNamespace):
         sf.Frame.from_concat((ns.frame, ns.frame, ns.frame),
                 axis=0,
@@ -43,6 +45,7 @@ class Prototype:
     # from_dict: same as from_items
 
     #---------------------------------------------------------------------------
+    # basic iteration
 
     def asv_time_iter_array_0(self, ns: SimpleNamespace):
         for part in ns.frame.iter_array(0):
@@ -94,6 +97,63 @@ class Prototype:
         for part in ns.frame.iter_element():
             pass
 
+    #---------------------------------------------------------------------------
+    # frame attrs, utility methods
+
+    def asv_time_values(self, ns: SimpleNamespace):
+        _ = ns.frame.values
+
+    def asv_time_display(self, ns: SimpleNamespace):
+        _ = ns.frame.display()
+
+    def asv_time_astype_element(self, ns: SimpleNamespace):
+        _ = ns.frame.astype(str)
+
+    # might try astype with iterable
+
+    #---------------------------------------------------------------------------
+    # frame selection
+
+    def asv_time_getitem_element(self, ns: SimpleNamespace):
+        _ = ns.frame[ns.columns_list[0]]
+
+    def asv_time_getitem_list(self, ns: SimpleNamespace):
+        _ = ns.frame[ns.columns_list]
+
+    def asv_time_getitem_slice(self, ns: SimpleNamespace):
+        _ = ns.frame[ns.columns_slice]
+
+    def asv_time_getitem_bool(self, ns: SimpleNamespace):
+        _ = ns.frame[ns.columns_bool]
+
+
+    def asv_time_loc_element_0(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_list[0]]
+
+    def asv_time_loc_list_0(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_list]
+
+    def asv_time_loc_slice_0(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_slice]
+
+    def asv_time_loc_bool_0(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_bool]
+
+
+    def asv_time_loc_element_1(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_list[0], ns.columns_list[0]]
+
+    def asv_time_loc_list_1(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_list, ns.columns_list]
+
+    def asv_time_loc_slice_1(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_slice, ns.columns_slice]
+
+    def asv_time_loc_bool_1(self, ns: SimpleNamespace):
+        _ = ns.frame.loc[ns.index_bool, ns.columns_bool]
+
+    # frame.bloc
+
 
 
 # NOTE: could not get setup_cache to work unless implemented explicitly on the derived class
@@ -107,10 +167,26 @@ def create_fixtures(fixture: str, shape: ShapeType):
     records_dict = [dict(t) for t in frame.iter_series(axis=1)]
     items = [(c, frame[c].values) for c in frame.columns]
 
-    return SimpleNamespace(records=records,
+    columns_list = [c for i, c in enumerate(frame.columns) if i % 2]
+    columns_slice = slice(frame.columns.iloc[len(frame.columns) // 2], None)
+    columns_bool = frame.columns.isin(columns_list)
+
+    index_list = [c for i, c in enumerate(frame.index) if i % 2]
+    index_slice = slice(frame.index.iloc[len(frame.index) // 2], None)
+    index_bool = frame.index.isin(index_list)
+
+    return SimpleNamespace(
+            frame=frame,
+            records=records,
             records_dict=records_dict,
             items=items,
-            frame=frame)
+            columns_list=columns_list,
+            columns_slice=columns_slice,
+            columns_bool=columns_bool,
+            index_list=index_list,
+            index_slice=index_slice,
+            index_bool=index_bool,
+            )
 
 @apply_prototype(Prototype)
 class FrameA:
--- benchmarks/frame_numeric.py
+++ benchmarks/frame_numeric.py
@@ -8,11 +8,93 @@ from .prototype import apply_prototype
 class Prototype:
 
     #---------------------------------------------------------------------------
+    # arithmetic methods
+
     def asv_time_sum_0(self, ns: SimpleNamespace):
         _ = ns.frame.sum(axis=0)
 
     def asv_time_sum_1(self, ns: SimpleNamespace):
         _ = ns.frame.sum(axis=1)
+
+    def asv_time_cumsum_0(self, ns: SimpleNamespace):
+        _ = ns.frame.cumsum(axis=0)
+
+    def asv_time_cumsum_1(self, ns: SimpleNamespace):
+        _ = ns.frame.cumsum(axis=1)
+
+    def asv_time_any_0(self, ns: SimpleNamespace):
+        _ = ns.frame.any(axis=0)
+
+    def asv_time_any_1(self, ns: SimpleNamespace):
+        _ = ns.frame.any(axis=1)
+
+
+    #---------------------------------------------------------------------------
+    # nan methods
+
+    def asv_time_isna(self, ns: SimpleNamespace):
+        _ = ns.frame.isna()
+
+    def asv_time_notna(self, ns: SimpleNamespace):
+        _ = ns.frame.notna()
+
+
+    def asv_time_dropna_0(self, ns: SimpleNamespace):
+        _ = ns.frame.dropna(axis=0)
+
+    def asv_time_dropna_1(self, ns: SimpleNamespace):
+        _ = ns.frame.dropna(axis=1)
+
+
+    def asv_time_fillna(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna(None)
+
+
+    def asv_time_fillna_leading_0(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_leading(None, axis=0)
+
+    def asv_time_fillna_leading_1(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_leading(None, axis=1)
+
+
+    def asv_time_fillna_trailing_0(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_trailing(None, axis=0)
+
+    def asv_time_fillna_trailing_1(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_trailing(None, axis=1)
+
+
+    def asv_time_fillna_forward_0(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_forward(axis=0)
+
+    def asv_time_fillna_forward_1(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_forward(axis=1)
+
+
+    def asv_time_fillna_backward_0(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_backward(axis=0)
+
+    def asv_time_fillna_backward_1(self, ns: SimpleNamespace):
+        _ = ns.frame.fillna_backward(axis=1)
+
+    #---------------------------------------------------------------------------
+    # unary, binary operators
+
+    def asv_time_unary_abs(self, ns: SimpleNamespace):
+        _ = abs(ns.frame)
+
+    def asv_time_binary_add_element(self, ns: SimpleNamespace):
+        _ = ns.frame + 10
+
+    def asv_time_binary_add_array(self, ns: SimpleNamespace):
+        _ = ns.frame + ns.frame.values
+
+    def asv_time_binary_eq_element(self, ns: SimpleNamespace):
+        _ = ns.frame == 10
+
+    def asv_time_binary_eq_array(self, ns: SimpleNamespace):
+        _ = ns.frame == ns.frame.values
+
 
 
 
@@ -35,7 +117,7 @@ class FrameA:
 @apply_prototype(Prototype)
 class FrameB:
 
-    FIXTURE = 'f(F)|i(I,str)|c(I,str)|v(int,int,bool,bool,float,float)'
+    FIXTURE = 'f(F)|i(I,str)|c(I,str)|v(int,int,bool,float,float,float)'
     SHAPE = (1000, 100)
 
     def setup_cache(self) -> SimpleNamespace:
