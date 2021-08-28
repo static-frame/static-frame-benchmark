3c944a88f9a30fafb3f986a020241a59d527f545
Christopher Ariza <ariza@flexatone.com>
2021-08-28 12:25:25 -0700

    updated tasks.py

--- tasks.py
+++ tasks.py
@@ -14,4 +14,23 @@ def clean(context):
     context.run('rm -rf .pytest_cache')
     context.run('rm -rf docs')
 
+@invoke.task
+def bench08(context):
+    '''Clean doc and build artifacts
+    '''
+    context.run('asv run v0.8.18..master --skip-existing')
+
+
+@invoke.task
+def publish(context):
+    context.run('asv publish')
+
+@invoke.task
+def preview(context):
+    context.run('asv preview')
+
+
+@invoke.task(pre=(bench08, publish, preview))
+def bench_pub_preview(context):
+    pass
 
